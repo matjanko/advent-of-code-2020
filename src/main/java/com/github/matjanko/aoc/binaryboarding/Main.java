3688ef0d10ce5398865b8deba4fdcfe89bb0cd7e
matjanko <mateusz.jankowski@prokonto.pl>
2020-12-06 21:49:33 +0100

    Day five part two

--- src/main/java/com/github/matjanko/aoc/binaryboarding/Main.java
+++ src/main/java/com/github/matjanko/aoc/binaryboarding/Main.java
@@ -36,5 +36,26 @@ public class Main {
                 .orElse(0);
 
         System.out.println(highestSeatID);
+
+        List<BoardingPass> boardingPassesWithoutFirstAndLastRow = boardingPasses.stream()
+                .filter(x -> x.getRow() != 0)
+                .filter(x -> x.getRow() != Plane.COLUMNS.size())
+                .collect(Collectors.toList());
+
+
+        List<Integer> seatIDs = boardingPassesWithoutFirstAndLastRow.stream()
+                .mapToInt(BoardingPass::getSeatID)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+        int mySeat = 0;
+        for (int i = 1; i < seatIDs.size() - 1; i++) {
+            if (!seatIDs.get(i).equals(seatIDs.get(i + 1) - 1)) {
+                mySeat = seatIDs.get(i) + 1;
+                break;
+            }
+        }
+        System.out.println(mySeat);
     }
 }
